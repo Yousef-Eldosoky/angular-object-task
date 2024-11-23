31c8792827ba6b6c4b2c8d5c8f81e12bf9edf5c3
Yousef Eldosoky <eldosoky@yousefsite.com>
2024-11-23 16:30:43 +0200

    use for loop to print country in <ul> <li>

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
