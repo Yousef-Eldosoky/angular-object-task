8848350e03d76139987a002c41051e07218cbe2e
Yousef Eldosoky <eldosoky@yousefsite.com>
2024-11-24 13:30:34 +0200

    update to angular 19

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,10 @@ import { NgFor, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common'
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [NgFor, NgSwitchCase, NgSwitch, NgSwitchDefault],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    imports: [NgFor, NgSwitchCase, NgSwitch, NgSwitchDefault],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css'
 })
 export class HomeComponent {
   peopleByCountry: any[] = [
